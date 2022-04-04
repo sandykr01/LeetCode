123b83d3181520ef4e1b9dcffce1b2c39d0d7485
sk9873422561 <68028455+sk9873422561@users.noreply.github.com>
2022-04-04 17:04:22 +0530

    Time: 14 ms (13.29%), Space: 43.1 MB (31.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -4,11 +4,7 @@ class Solution {
         HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,i);
-            }else{
-                map.put(ch,i);
-            }
+            map.put(ch,i);
         }
         
         // System.out.println(map);
@@ -17,11 +13,10 @@ class Solution {
         int pre = -1;
         int curr = 0;
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            int reachOfCurr = map.get(ch);
-            if(reachOfCurr>curr){
-                curr = reachOfCurr;
-            }
+            
+            int reachOfCurr = map.get(s.charAt(i));
+            curr = Math.max(curr,reachOfCurr);
+                
             if(curr==i){
                 res.add(curr-pre);
                 pre = curr;
