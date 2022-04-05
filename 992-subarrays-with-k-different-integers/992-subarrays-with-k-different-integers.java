c078cc03c6bdb3c150ef7ac344f72ebc616f8076
sk9873422561 <68028455+sk9873422561@users.noreply.github.com>
2022-04-06 00:19:21 +0530

    Time: 86 ms (35.10%), Space: 73.4 MB (22.29%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int getSubArrayWithAtMostK(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        int i=0;
+        int j=0;
+        while(i<nums.length){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            while(map.size()>k){
+                map.put(nums[j],map.get(nums[j])-1);
+                if(map.get(nums[j])==0) map.remove(nums[j]);
+                j++;
+            }
+            count+= i-j+1;
+            i++;
+        }
+        return count;
+    }
+    
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int res = getSubArrayWithAtMostK(nums,k)-getSubArrayWithAtMostK(nums,k-1);
+        return res;
+    }
+}
