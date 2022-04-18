4a5c5adbfa3e031bb9eda7868ed0ab7e0ea98c92
sk9873422561 <68028455+sk9873422561@users.noreply.github.com>
2022-04-18 14:02:31 +0530

    Time: 37 ms (59.35%), Space: 44 MB (83.54%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public int[] nextGreaterRight(int[] nums){
+    public int[] nextGreaterRightCircular(int[] nums){
         int n = nums.length;
         int[] ans = new int[nums.length];
         Arrays.fill(ans, -1);
@@ -10,7 +10,7 @@ class Solution {
         
         for(int i=1;i<n*2;i++){
             while(st.size()>0 && nums[st.peek()]<nums[i%n]){
-                ans[st.pop()] = i%n;
+                ans[st.pop()] = nums[i%n];
             }
             st.push(i%n);
         }
@@ -20,16 +20,9 @@ class Solution {
     
     
     public int[] nextGreaterElements(int[] nums) {
-        int[] ngRight = nextGreaterRight(nums);
+        int[] ngRight = nextGreaterRightCircular(nums);
         
-        int[] nextGreater = new int[nums.length];
-        
-        for(int i=0;i<nums.length;i++){
-            if(ngRight[i]>=0) nextGreater[i] = nums[ngRight[i]];
-            else nextGreater[i] = -1;
-        }
-        
-        return nextGreater;
+        return ngRight;
         
     }
 }
