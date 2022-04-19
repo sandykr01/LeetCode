a597d9353e97c73274b5f3b58b6710a35d7f3029
sk9873422561 <68028455+sk9873422561@users.noreply.github.com>
2022-04-19 19:29:36 +0530

    Time: 1 ms (53.16%), Space: 41.8 MB (72.52%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -20,7 +20,7 @@ class Solution {
         if(node1==node && node2==node) return true; // stop traversal in mid you don't need to travel further
         
         boolean res = (node1==null || node2==null) ? false : node1.val==node2.val;
-        if(res==false) return res; // return as you get false no need to check further
+        if(res==false) return res;
         boolean lans = doubleTraversal(node,node1.left,node2.right);
         boolean rans = doubleTraversal(node,node1.right,node2.left);
         
