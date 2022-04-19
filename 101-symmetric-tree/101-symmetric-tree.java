da1bf80d837cbe3542f0c68cf519dda1f7d236af
sk9873422561 <68028455+sk9873422561@users.noreply.github.com>
2022-04-19 19:28:04 +0530

    Time: 1 ms (53.16%), Space: 42.3 MB (48.03%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,11 +16,11 @@
 class Solution {
     
     public boolean doubleTraversal(TreeNode node, TreeNode node1, TreeNode node2){
-        if(node1==null && node2==null) return true; // node 1 and node2 will become null together
+        if(node1==null || node2==null) return node1==node2; // node 1 and node2 will become null together
         if(node1==node && node2==node) return true; // stop traversal in mid you don't need to travel further
         
-        boolean res = (node1==null || node2==null) ? false : node1.val==node2.val;
-        if(res==false) return res;
+        boolean res = node1.val==node2.val;
+        if(res==false) return res; // return as you get false no need to check further
         boolean lans = doubleTraversal(node,node1.left,node2.right);
         boolean rans = doubleTraversal(node,node1.right,node2.left);
         
